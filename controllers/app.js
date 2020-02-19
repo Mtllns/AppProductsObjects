685feae327d44782eeb4aa88df633b0234e10208
Mtllns <miquelmtllns@pm.me>
2020-02-19 20:18:23 +0100

    Añado la App en MVC
    MVC + fin de App

---
+++ controllers/app.js
@@ -0,0 +1,30 @@
+
+// DOM Events
+document.getElementById('product-form').addEventListener("submit", function(e) { // function(e) la "e" viene de "event" que se utiliza más a bajo con un metodo.
+    const name  = document.getElementById('name').value;
+    const price = document.getElementById('price').valueAsNumber;
+    const year  = document.getElementById('year').value;
+    const ui = new UI(); // instaciamos objeto UserInterface "UI"
+
+    if ((name == "") || (price == "") || (year == "")) { // comprobamos si los campos estan vacios
+        ui.showMessage('No se puede crear un Producto vacio, rellena los campos, por favor', 'danger'); // mensaje de alerta para que rellene los campos
+    }else {
+        console.log("Name: "+ name + "\n" + "Price: " + price + "\n" + "Year: " + year);
+        console.log(new Product(name, price, year));
+    
+        const product = new Product(name, price, year); // instaciamos objeto producto
+        
+        ui.addProduct(product);
+        ui.resetForm(); // llamamos al metodo para limpiar el formulario
+        ui.showMessage('Product Added Successfully', 'success'); // llamamos metodo para mostrar mensaje
+    
+    }
+    e.preventDefault(); // el metodo "preventDefault()" detiene el comportamiento por defecto del "sumbit"
+
+});
+// recogemos evento "click en el DOM, en el elemento con la clase 'product-list'"
+document.getElementById('product-list').addEventListener('click', function(e){ // function(e) la "e" viene de "event"
+    const ui = new UI();
+    ui.deleteProduct(e.target);
+    console.log(e.target);
+});
---
+++ model/ui.js
@@ -0,0 +1,44 @@
+class UI {
+    // metodo para añadir producto
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = 
+            `<div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product Name</strong>: ${product.name}
+                    <strong>Product Price</strong>: ${product.price}€
+                    <strong>Product Year</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete">Delete</a>
+                </div>
+            </div>`;
+
+        productList.appendChild(element); // al div 'product-list' le añadimos un elemento html hijo
+        this.resetForm(); //
+    }
+    // metodo para eliminar datos en el formulario despues de crear el producto
+    resetForm() { 
+        document.getElementById('product-form').reset();
+    }
+    // metodo para eliminar producto
+    deleteProduct(element) {
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product Delete Successfully', 'warning');
+        }
+    }
+    // metodo para mostrar mensaje cuando creamos producto, eliminamos o datos vacios.
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message));
+        // Showing in DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#app');
+        container.insertBefore(div, app);
+        // con el siguiente metodo eliminamos el mensaje despúes de 3seg
+        setTimeout(function() {
+            document.querySelector('.alert').remove(); 
+        }, 4000);
+    }   
+}
